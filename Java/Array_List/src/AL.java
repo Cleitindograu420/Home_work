2ebf42ef9976c4a7051764272527f52d61cc34a0
Mezzano <166826256+Cleitindograu420@users.noreply.github.com>
2024-09-25 00:52:59 -0300

    Update AL.java

--- Java/Array_List/src/AL.java
+++ Java/Array_List/src/AL.java
@@ -1,30 +1,43 @@
-
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-public class AL {
-
+public class atividade {
     public static void main(String[] args) {
-        String nrElementos = JOptionPane.showInputDialog("Quantos numeros a lista vai ter");
-
-        int nElementos = Integer.valueOf(nrElementos);
-
+        
+        //cria a lista
         ArrayList<Integer> lista = new ArrayList<Integer>();
-
-        for (int i = 0; i < nElementos; i++) {
-            String valor = JOptionPane.showInputDialog("Informe o valor " + i);
+        String nrElementos = JOptionPane.showInputDialog("Quantos nrs a lista vai ter?");
+        int nElementos = Integer.valueOf(nrElementos);
+        
+        //Repetição respónsavel por capturar os valores digitados pelo usuario e adicioná los na lista
+        for(int i = 0; i< nElementos; i++) {
+            String valor =
+                    JOptionPane.showInputDialog("Informe o valor: "+ i);
             lista.add(Integer.valueOf(valor));
         }
-        System.out.println(lista);
-
+        
         int soma = 0;
-        for (int i = 0; i < lista.size(); i++) {
+        for(int i = 0; i< lista.size(); i ++) {
             soma = soma + lista.get(i);
         }
-        System.out.println(soma);
-
-        int soma_par = 0;
-        for ( )
-
-    }
+        System.out.println("Soma dos Elementos: "+ soma);
+        
+        int soma = 0;
+        int pares = 0;
+        int indicesimpares = 0;
+        int elemento = 0;
+        for(int i = 0; i < lista.size(); i++) {
+            elemento = lista.get(i);
+            soma = soma + elemento;
+            }
+            if(elemento % 2 ==0) {
+                pares = pares + elemento;
+            }
+            if(elemento % 2 ==1) {
+                indicesimpares = indicesimpares + elemento;
+        }
+            System.out.println("Soma dos Elementos: "+ soma);
+            System.out.println("Soma dos pares: "+ pares);
+            System.out.println("Soma dos impares: "+ indicesimpares);
+            
 }
