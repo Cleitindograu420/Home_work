31df96a1642e9a380131cce5938e73c499ae9499
Mezzano <166826256+Cleitindograu420@users.noreply.github.com>
2024-09-26 16:26:47 -0300

    php basics

---
+++ php/variaveis.php
@@ -0,0 +1,59 @@
+<?php
+
+//variavel nome
+$nome = "eu te pego pela cabeça";
+$sobrenome = "e te jooj";
+
+///////////////////////////////////////////////
+
+//deu "print" na variavel nome
+//echo $nome;
+
+///////////////////////////////////////////////
+
+//retorna o tipo da variavel e o numero de caracteres
+//var_dump($nome);
+
+///////////////////////////////////////////////
+
+//variavel não pode começar com numero e caracter expecial apenas "_"
+//$1nome da erro
+//$nome1 não da erro
+
+///////////////////////////////////////////////
+
+//"<br/>" quebra de linha
+//echo "<br/>";
+
+///////////////////////////////////////////////
+
+//tira o que tem dentro de uma ou mais variaveis
+//unset($nome);
+
+///////////////////////////////////////////////
+
+//concatenação com um espaço
+//$nomeCompleto = $nome . " " . $sobrenome;
+//echo $nomeCompleto;
+
+///////////////////////////////////////////////
+
+//tipos de dados
+//$nome = "Chavo del 8";
+//$site = 'Rojerin.com.br';
+//$ano = 2000;
+//$salario = 5500.90;
+//$block = false;
+
+///////////////////////////////////////////////
+
+//$frutas = array("laranja", "abacaxi", "banana")
+//$horario = new DateTime();
+
+///////////////////////////////////////////////
+
+//$arquivo = fopen("index.php", "r");
+//NULL ≠ " "
+//$nulo = NULL;
+
+?>
