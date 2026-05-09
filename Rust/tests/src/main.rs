2a8788c151b6406adb2fa704126cd20288e8abb6
Mezzano <ricoribeiroreis@gmail.com>
2026-05-09 18:38:13 -0300

    Update main.rs

--- Rust/tests/src/main.rs
+++ Rust/tests/src/main.rs
@@ -1,11 +1,3 @@
-fn media(vector: Vec<f64>) -> f64 {
-    let sum: f64 = vector.iter().sum();
-    let count = vector.len() as f64;
-    return sum / count
-}
-
 fn main(){
-    let numbers = vec![1.0, 2.0, 3.0, 4.0, 5.0];
-    let result = media(numbers);
-    println!("A média é: {}", result);
+    println!("Hello, world!");
 }
